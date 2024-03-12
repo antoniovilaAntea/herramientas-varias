c59c78edc8529185c15fe01dc5c38073f450ef51
antoniovila <antoniovila@anteagroup.es>
2024-03-12 17:38:38 +0100

    add: add link to download the zip with java project

--- src/Pages/Emails/emails.tsx
+++ src/Pages/Emails/emails.tsx
@@ -5,7 +5,7 @@ const Emails = () => {
     <>
       <h3>Descargar Generador Emails</h3>
       <a
-        href="/Emails_File/Creador de EmailS de gomas Pontevedra (tanto operarios como aytos).zip"
+        href="https://iceacsaconsultores-my.sharepoint.com/:u:/g/personal/antoniovila_anteagroup_es/ESCk61grpIdEiEc2t-woFiUB-OzdT8vCYsI3yH1ORRZ6wA?e=Rg2UpU"
         download={"Generador Emails"}
         target="_blank"
         rel="noreferrer"
