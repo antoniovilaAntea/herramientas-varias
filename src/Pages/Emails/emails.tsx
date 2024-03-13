650e70d0da1a3936ab7690904da8a5058eac19bb
antoniovila <antoniovila@anteagroup.es>
2024-03-13 17:39:11 +0100

    style: update style

--- src/Pages/Emails/emails.tsx
+++ src/Pages/Emails/emails.tsx
@@ -1,17 +1,39 @@
 import React from "react";
+import EmailGenerator from "../../Components/EmailGenerator";
 
 const Emails = () => {
   return (
     <>
-      <h3>Descargar Generador Emails</h3>
-      <a
-        href="https://iceacsaconsultores-my.sharepoint.com/:u:/g/personal/antoniovila_anteagroup_es/ESCk61grpIdEiEc2t-woFiUB-OzdT8vCYsI3yH1ORRZ6wA?e=Rg2UpU"
-        download={"Generador Emails"}
-        target="_blank"
-        rel="noreferrer"
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginBottom: "2em",
+        }}
       >
-        <button>Descargar .zip</button>
-      </a>
+        <h3>Descargar Generador Emails</h3>
+        <a
+          href="https://iceacsaconsultores-my.sharepoint.com/:u:/g/personal/antoniovila_anteagroup_es/ESCk61grpIdEiEc2t-woFiUB-OzdT8vCYsI3yH1ORRZ6wA?e=Rg2UpU"
+          download={"Generador Emails"}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button>Descargar .zip</button>
+        </a>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h3>Generar email - Dep. Pontevedra</h3>
+        <EmailGenerator></EmailGenerator>
+      </div>
     </>
   );
 };
