2f52d4c1a9fab0d6ed94b18a389f1ab199f775f1
antoniovila <antoniovila@anteagroup.es>
2024-03-14 09:43:15 +0100

    add: finish help page

---
+++ src/Pages/Ayuda/Ayuda.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Ayuda = () => {
+  return (
+    <div>
+      <div>
+        <h3>Conversor de .txt a .af1</h3>
+        <p>
+          En esta página hay dos opciones, o <b>descargar el zip</b> o,{" "}
+          <b>convertir el archivo online</b>.
+        </p>
+        <p>
+          La primera opción, redirige a un archivo comprimido en Onedrive donde
+          está todo el instalable y ejecutable del Conversor
+        </p>
+        <p>
+          La segunda opción permite la conversión del archivo subiendolo a la
+          propia página
+        </p>
+      </div>
+      <div>
+        <h3>Unificador de archivo de aforos</h3>
+        <p>
+          Hay dos opciones, o <b>descargar el zip</b> o,{" "}
+          <b>unir los dos archivos desde la misma página web</b>.
+        </p>
+        <p>
+          La primera opción, redirige a un archivo comprimido en Onedrive donde
+          está todo el instalable y ejecutable del Unificador
+        </p>
+        <p>
+          La segunda opción permite unir, subiendo los dos archivos a la web y
+          haciendo clic en el botón de unir, mediante la página web, totalmente
+          online
+        </p>
+      </div>
+      <div>
+        <h3>
+          Generador de Emails para operarios y Ayuntamientos - Dep. Pontevedra
+        </h3>
+        <p>
+          Hay dos opciones, o <b>descargar el zip</b> o,{" "}
+          <b>generar el texto del email de manera online</b>.
+        </p>
+        <p>
+          La primera opción, redirige a un archivo comprimido en Onedrive donde
+          está todo el instalable y ejecutable del Generador de Emails
+        </p>
+        <p>
+          La segunda opción permite generar de manera online el cuerpo del
+          email. Hay que subir el Excel con todos los datos de las gomas de la
+          <br />
+          Dep. Pontevedra, introducir la semana, la fecha y seleccionar el tipo,
+          si instalar o retirar, y luego seleccionar en la lista las gomas que
+          <br />
+          se deseen, pulsar añadir, para poder seguir seleccionando otros dias y
+          tipos y pulsar el botón exportar
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Ayuda;
