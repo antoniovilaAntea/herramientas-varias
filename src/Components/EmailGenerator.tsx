21c8dba162cb115411ea4585a9a3e08241d1742d
antoniovila <antoniovila@anteagroup.es>
2024-03-26 10:43:00 +0100

    add: deseleccionar button

--- src/Components/EmailGenerator.tsx
+++ src/Components/EmailGenerator.tsx
@@ -6,15 +6,14 @@ import "./emailGenerator.css";
 
 function EmailGenerator() {
   const [excelData, setExcelData] = useState<any[]>([]);
-  //variables a guardar para pasar al archivo word
+  const [mostrarBotones, setMostrarBotones] = useState(false);
   const [selectedRows, setSelectedRows] = useState<any[]>([]);
   const [instalar, setInstalar] = useState(false);
   const [retirar, setRetirar] = useState(false);
   const [inicioSemana, setInicioSemana] = useState<string>();
   const [finSemana, setFinSemana] = useState<string>();
   const [fecha, setFecha] = useState<string>();
-
-  const [datos, setDatos] = useState([{}]);
+  const [datos, setDatos] = useState<any[]>([]);
 
   const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files && e.target.files[0];
@@ -59,12 +58,64 @@ function EmailGenerator() {
     }
   };
 
-  const exportarDatos = () => {
-    // AQUI HAY QUE PONER EL CODIGO PARA EXPORTAR A UN ARCHIVO DE WORD
-    console.log(datos);
-    //El filter de abajo lo que hace es devovler todos los elementos que CONTENGAN en fecha ' Lunes 12'
-    // @ts-ignore
-    console.log(datos.filter((elemento) => elemento.fecha === "Lunes 12"));
+  const handleSendEmail = () => {
+    const emailBody = createEmailBody(datos);
+    openOutlook(emailBody);
+  };
+
+  const createEmailBody = (data: any[]) => {
+    let emailBody = "\n\n";
+
+    const groupedData: { [key: string]: any[][] } = {};
+    data.forEach((item) => {
+      const key = `${item.fecha}-${item.tipo}`;
+      if (!groupedData[key]) {
+        groupedData[key] = [];
+      }
+      groupedData[key] = groupedData[key].concat(item.selectedRows);
+    });
+
+    let lastDate = "";
+    Object.keys(groupedData).forEach((key, index) => {
+      const [fecha, tipo] = key.split("-");
+      if (fecha !== lastDate) {
+        emailBody += `${fecha.toUpperCase()}\n\n`;
+        lastDate = fecha;
+      }
+      emailBody += `Gomas a ${tipo.toString().toUpperCase()}:\n`;
+      groupedData[key].forEach((row: any) => {
+        emailBody += row.join(" - ") + "\n";
+      });
+
+      if (index < Object.keys(groupedData).length - 1) {
+        emailBody += "\n";
+      }
+    });
+
+    return encodeURIComponent(emailBody);
+  };
+  const openOutlook = (emailBody: string) => {
+    const emailSubject = encodeURIComponent(
+      `PROGRAMACIÓN AFOROS DEPO ${new Date().getFullYear()}, SEMANA DEL ${datos
+        .map((e) => e.inicioSemana)
+        .filter((value, index, self) => self.indexOf(value) === index) // Filtrar fechas duplicadas
+        .join(", ")} AL ${datos
+        .map((e) => e.finSemana)
+        .filter((value, index, self) => self.indexOf(value) === index) // Filtrar fechas duplicadas
+        .join(", ")}:`
+    );
+
+    const mailtoLink = `mailto:?subject=${emailSubject}&body=${emailBody}`;
+
+    window.location.href = mailtoLink;
+  };
+
+  const manjearBotones = () => {
+    setMostrarBotones(!mostrarBotones);
+  };
+
+  const unselectAll = () => {
+    setSelectedRows([]);
   };
 
   const anadirDatos = () => {
@@ -74,14 +125,15 @@ function EmailGenerator() {
         JSON.stringify(obj) ===
         JSON.stringify({ inicioSemana, finSemana, fecha, tipo, selectedRows })
     );
-    !hayDuplicadosEnDatos
-      ? setDatos([
-          ...datos,
-          { inicioSemana, finSemana, fecha, tipo, selectedRows },
-        ])
-      : alert("Ya hay un elemento igual");
+    if (!hayDuplicadosEnDatos) {
+      setDatos([
+        ...datos,
+        { inicioSemana, finSemana, fecha, tipo, selectedRows },
+      ]);
+    } else {
+      alert("Ya hay un elemento igual");
+    }
 
-    console.log(datos);
     return hayDuplicadosEnDatos;
   };
 
@@ -98,14 +150,14 @@ function EmailGenerator() {
             placeholder="dd/MM"
             type="text"
             value={inicioSemana}
-            onChange={(e) => setInicioSemana(e.target.value!)}
+            onChange={(e) => setInicioSemana(e.target.value)}
           />
           <label> a </label>
           <input
             placeholder="dd/MM"
             type="text"
             value={finSemana}
-            onChange={(e) => setFinSemana(e.target.value!)}
+            onChange={(e) => setFinSemana(e.target.value)}
           />
         </div>
         <div className="fecha">
@@ -114,7 +166,7 @@ function EmailGenerator() {
             type="text"
             placeholder="Ej: Lunes 12"
             value={fecha}
-            onChange={(e) => setFecha(e.target.value!)}
+            onChange={(e) => setFecha(e.target.value)}
           />
           <div className="checkboxes">
             <input
@@ -152,8 +204,17 @@ function EmailGenerator() {
           >
             Añadir
           </button>
-          <button onClick={exportarDatos}>Exportar</button>
+          <button onClick={manjearBotones}>Exportar</button>
+          {mostrarBotones && (
+            <div className="btnAux">
+              <button onClick={handleSendEmail}>Email a Operarios</button>{" "}
+              <button>Email a Ayuntamientos</button>
+            </div>
+          )}
         </div>
+      </div>
+      <div className="deseleccionar">
+        <button onClick={() => unselectAll()}>Deseleccionar todos</button>
       </div>
       <table>
         <thead>
