52fcbe6d946df28f2beffb43c14a51bb07145ab2
antoniovila <antoniovila@anteagroup.es>
2024-04-02 11:10:12 +0200

    feat: add style to show what menu is selected

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -1,5 +1,5 @@
 // Navigation.tsx
-import React from "react";
+import React, { useState } from "react";
 import { Route, Link, Routes } from "react-router-dom";
 import Inicio from "../Pages/Inicio";
 import Conversor from "../Pages/Conversor/conversor";
@@ -10,15 +10,41 @@ import Ayuda from "../Pages/Ayuda/Ayuda";
 import "./estilo.css";
 
 const Navigation = () => {
+  const [activeLink, setActiveLink] = useState("");
+
   return (
     <>
-      <header>Herramientas variadas</header>
+      <header>Herramientas</header>
       <div className="menu">
         <nav className="menu__nav">
-          <Link to="/">Inicio</Link>
-          <Link to="/conversor">Conversor</Link>
-          <Link to="/unificador">Unificador</Link>
-          <Link to="/email">Email</Link>
+          <Link
+            to="/"
+            onClick={() => setActiveLink("inicio")}
+            className={activeLink === "inicio" ? "bold" : ""}
+          >
+            Inicio
+          </Link>
+          <Link
+            to="/conversor"
+            onClick={() => setActiveLink("conversor")}
+            className={activeLink === "conversor" ? "bold" : ""}
+          >
+            Conversor
+          </Link>
+          <Link
+            to="/unificador"
+            onClick={() => setActiveLink("unificador")}
+            className={activeLink === "unificador" ? "bold" : ""}
+          >
+            Unificador
+          </Link>
+          <Link
+            to="/email"
+            onClick={() => setActiveLink("email")}
+            className={activeLink === "email" ? "bold" : ""}
+          >
+            Email
+          </Link>
         </nav>
         <Routes>
           <Route path="/" element={<Inicio />} />
