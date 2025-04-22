bd8134ae025ff09666dbd4f7bfc073a22e3c7eb7
antoniovilaAntea <antoniovila@anteagroup.es>
2025-04-22 17:11:15 +0200

    feat: add a little description to index page

--- src/Pages/Inicio.tsx
+++ src/Pages/Inicio.tsx
@@ -20,20 +20,29 @@ const Inicio = () => {
           <div className="descripciones">
             <ul className="descripciones__lista">
               <li>
-                <b>Conversor TXT a Aforo</b>: Esta aplicación sirve para
+                <b>Conversor Espiras-Gomas</b>: Esta aplicación sirve para
                 convertir el archivo txt que se recibe a un archivo af1 que se
                 necesita para los informe de aforos
               </li>
               <li>
-                <b>Unificador Aforo ida y Aforo vuelta</b>: Esta aplicación une
-                los dos archivos de aforos, de un carril de ida y un carril de
-                vuelta, en uno solo, con todos los datos
+                <b>Unificador sentidos</b>: Esta aplicación une los dos archivos
+                de aforos, de un carril de ida y un carril de vuelta, en uno
+                solo, con todos los datos
               </li>
               <li>
-                <b>Creador de emails gomas - D. Pontevedra</b>: Esta herramienta
-                sirve para generar los emails que se envían a las\los
-                trabajadoras\es y a los Ayuntamientos sobre las gomas a instalar
-                o retirar
+                <b>Listado de gomas</b>: Con esta herramienta podremos invertir
+                el Excel que se envía desde la Xunta de su manera a una forma
+                mucho más accesible y usable.
+              </li>
+              <li>
+                <b>Accidentes DEPO</b>: Esta herramienta sirve para comparar dos
+                excel con datos de los accidentes de la Diputación de Pontevedra
+                y, exportar los Datos en un mismo Excel
+              </li>
+              <li>
+                <b>Generador de emails - DEPO</b>: Esta herramienta sirve para
+                generar los emails que se envían a las\los trabajadoras\es y a
+                los Ayuntamientos sobre las gomas a instalar o retirar
               </li>
               <li>
                 <b>Buscador de celdas</b>: Esta herramienta sirve para buscar el
@@ -41,10 +50,10 @@ const Inicio = () => {
                 por comas y sacar un documento con estos valores
               </li>
               <li>
-                <b>Comparador de excel</b>: Esta herramienta sirve para comparar
-                dos excel con datos de los accidentes de la Diputación de
-                Pontevedra y, exportar los Datos en un mismo Excel (En
-                desarrollo)
+                <b>Festivos</b>: Esta herramienta sirve para consultar los
+                festivos de cada ayuntamiento de la Comunididad Autónoma de
+                Galicia. Además se consultan los festivos nacionales y los
+                autonómicos
               </li>
               <li>
                 <b>Conversor Excel a xml [Expropiaciones]</b>: Esta herramienta
